6b668e4f4dcdfc3287aee64615b632062de6110a
CIT142G-27@M306.UC-BCF.EDU.PH <CIT142G-27@M306.UC-BCF.EDU.PH>
2022-09-14 15:42:22 +0800

    Auto upload

--- push.bat
+++ push.bat
@@ -1,6 +1,5 @@
 @echo off
-git remote set-url origin
-https://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@github.com/RoookieLygie/CIT142G.git
+git remote set-url origin https://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@github.com/RoookieLygie/CIT142G.git
 pause
 git add .
 git commit -m "Auto upload"
